27fde7099738b2391bcfe3d8b10f2a65cfa238ff
Remington Cunov <61329286+rcunov@users.noreply.github.com>
2023-01-25 15:37:46 -0600

    Update login.sh

--- login.sh
+++ login.sh
@@ -3,7 +3,7 @@
 # purpose of this script 
 
 # declare vars
-WAIT_TIME=$(shuf -i 0-10 -n 1)  # selects a pseudo-random number from 0-10
+WAIT_TIME=1 # $(shuf -i 0-10 -n 1)  # selects a pseudo-random number from 0-10
 ENV_FILE=.env
 SITE_URL=$(cat $ENV_FILE | grep SITE_URL | rev | cut -f 1 -d '=' | rev)  # grabs the value of SITE_URL
 									 # from the env file declared above
@@ -11,4 +11,4 @@ SITE_URL=$(cat $ENV_FILE | grep SITE_URL | rev | cut -f 1 -d '=' | rev)  # grabs
 echo "Waiting for $WAIT_TIME seconds..."
 
 sleep $WAIT_TIME && echo "Logging in to $SITE_URL" \
-	&& docker run --env-file $ENV_FILE --shm-size 2g rcunov/selenium-autologin | tee log.txt
+	&& docker run --env-file $ENV_FILE --shm-size 2g rcunov/selenium-autologin | tee -a log.txt
